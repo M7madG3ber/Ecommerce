eb1ed009a3c00c6d4fcda834676bf4b62fd5bb2c
M7madG3ber <m7mad.g3ber@gmail.com>
2023-11-26 20:21:12 +0200

    Add dashboard assets

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,16 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('dashboard.layouts.layout')
 
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-</head>
+@section('title')
+Home | @parent
+@endsection
 
-<body>
-      <h1>Dashboard</h1>
-      <a href="{{ route('logout') }}">Logout</a>
-</body>
+@section('content')
 
-</html>
+<!-- Content Header -->
+<div class="content-header">
+      <div class="container-fluid">
+            <div class="row mb-2">
+                  <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                  </div>
+            </div>
+      </div>
+</div>
+<!-- Content Header -->
+
+<!-- Main content -->
+<div class="content">
+      <div class="container-fluid">
+            <div class="row">
+                  <div class="col-lg-6">
+                        <div class="card">
+                              <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                    <p class="card-text">
+                                          Some quick example text to build on the card title and make
+                                          up the bulk of the card's
+                                          content.
+                                    </p>
+
+                                    <a href="#" class="card-link">Card link</a>
+                                    <a href="#" class="card-link">Another link</a>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </div>
+</div>
+<!-- Main content -->
+
+@endsection
---
+++ resources/views/dashboard/layouts/layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
+      <title>
+            @section('title')
+            Dashboard
+            @show
+      </title>
+
+      <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+      <link rel="stylesheet" href="{{ asset('assets/dashboard/plugins/fontawesome-free/css/all.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('assets/dashboard/dist/css/adminlte.min.css') }}">
+      @yield('styles')
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+      <div class="wrapper">
+
+            {{-- @include('dashboard.layouts.nav') --}}
+
+            @include('dashboard.layouts.aside')
+
+            <div class="content-wrapper">
+                  @yield('content')
+            </div>
+
+            {{-- @include('dashboard.layouts.footer') --}}
+      </div>
+
+      <script src="{{ asset('assets/dashboard/plugins/jquery/jquery.min.js') }}"></script>
+      <script src="{{ asset('assets/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('assets/dashboard/dist/js/adminlte.min.js') }}"></script>
+      @yield('scripts')
+
+</body>
+
+</html>
